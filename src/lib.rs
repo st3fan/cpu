fc4aeebc1d5af391009ca4239d0c948df45e9271
Stefan Arentz <stefan@arentz.ca>
2023-08-25 17:03:53 -0400

    Calling functions

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,15 @@ struct CPU {
 #[allow(dead_code)]
 impl CPU {
     fn new() -> Self {
-        CPU { a: 0, x: 0 ,y: 0, pc: 0x0400, s: 0xff, p: 0x00, mem: [0; 2048] }
+        CPU {
+            a: 0,
+            x: 0,
+            y: 0,
+            pc: 0x0400,
+            s: 0xff,
+            p: 0x00,
+            mem: [0; 2048],
+        }
     }
 
     fn read_byte(&mut self) -> u8 {
@@ -46,23 +54,75 @@ impl CPU {
         b
     }
 
+    fn read_word(&mut self) -> u16 {
+        (self.read_byte() as u16) << 8 | self.read_byte() as u16
+    }
+
+    fn push_byte(&mut self, b: u8) {
+        self.mem[(0x0100 + self.s as u16) as usize] = b;
+        self.s -= 1;
+    }
+
+    fn pop_byte(&mut self) -> u8 {
+        self.s += 1;
+        let b = self.mem[(0x0100 + self.s as u16) as usize];
+        b
+    }
+
+    fn push_word(&mut self, w: u16) {
+        self.push_byte((w >> 8) as u8);
+        self.push_byte((w & 0x00ff) as u8);
+    }
+
+    fn pop_word(&mut self) -> u16 {
+        (self.pop_byte() as u16) | (self.pop_byte() as u16) << 8
+    }
+
     fn run(&mut self) {
         loop {
             let opcode = self.read_byte();
 
+            println!("Executing {:x}", opcode);
+
             match opcode {
-                0xA9 => { // LDA IMM
-                    let oper = self.read_byte();
-                    self.a = oper;
+                0x00 => {
+                    // BRK
+                    return;
                 }
 
-                0x85 => { // STA ZP
+                0x20 => {
+                    // JSR ABS
+                    self.push_word(self.pc + 2);
+                    self.pc = self.read_word();
+                }
+
+                0x60 => {
+                    self.pc = self.pop_word();
+                }
+
+                0x85 => {
+                    // STA ZP
                     let oper = self.read_byte();
                     self.mem[oper as usize] = self.a;
                 }
 
-                0x00 => { // BRK
-                    return
+                0x86 => {
+                    // STX ZP
+                    let oper = self.read_byte();
+                    self.mem[oper as usize] = self.x;
+                }
+
+                0xA2 => {
+                    // LDX IMM
+                    self.x = self.read_byte();
+                }
+
+                0xA9 => {
+                    // LDA IMM
+                    self.a = self.read_byte();
+                }
+
+                0xEA => { // NOP
                 }
 
                 _ => {
@@ -89,5 +149,95 @@ mod tests {
         assert_eq!(0x0405, cpu.pc);
         assert_eq!(0x42, cpu.a);
         assert_eq!(0x42, cpu.mem[0x0007]);
+    }
+
+    #[test]
+    fn lda_imm() {
+        let mut cpu = CPU::new();
+        cpu.mem[0x0400] = 0xA9; // LDX #$42
+        cpu.mem[0x0401] = 0x42;
+        cpu.mem[0x0402] = 0x00;
+        cpu.run();
+        assert_eq!(0x42, cpu.a);
+    }
+
+    #[test]
+    fn sta_zp() {
+        let mut cpu = CPU::new();
+        cpu.a = 0x42;
+        cpu.mem[0x0400] = 0x85; // STA $07
+        cpu.mem[0x0401] = 0x07;
+        cpu.mem[0x0402] = 0x00;
+        cpu.run();
+        assert_eq!(0x42, cpu.mem[0x07]);
+    }
+
+    #[test]
+    fn ldx_imm() {
+        let mut cpu = CPU::new();
+        cpu.mem[0x0400] = 0xA2; // LDX #$65
+        cpu.mem[0x0401] = 0x65;
+        cpu.mem[0x0402] = 0x00;
+        cpu.run();
+        assert_eq!(0x65, cpu.x);
+    }
+
+    #[test]
+    fn stx_zp() {
+        let mut cpu = CPU::new();
+        cpu.x = 0x42;
+        cpu.mem[0x0400] = 0x86; // STX $07
+        cpu.mem[0x0401] = 0x07;
+        cpu.mem[0x0402] = 0x00;
+        cpu.run();
+        assert_eq!(0x42, cpu.mem[0x07]);
+    }
+
+    #[test]
+    fn push_pop_byte() {
+        let mut cpu = CPU::new();
+
+        cpu.push_byte(0x11);
+        assert_eq!(0x11, cpu.mem[0x01ff]);
+        assert_eq!(0xfe, cpu.s);
+
+        let b = cpu.pop_byte();
+        assert_eq!(0x11, b);
+        assert_eq!(0xff, cpu.s);
+        assert_eq!(0x11, cpu.mem[0x01ff]);
+    }
+
+    #[test]
+    fn push_pop_word() {
+        let mut cpu = CPU::new();
+        cpu.push_word(0x1234);
+        assert_eq!(0x12, cpu.mem[0x01ff]);
+        assert_eq!(0x34, cpu.mem[0x01fe]);
+        assert_eq!(0xfd, cpu.s);
+        let w = cpu.pop_word();
+        assert_eq!(0x1234, w);
+        assert_eq!(0xff, cpu.s);
+    }
+
+    #[test]
+    fn call_and_return() {
+        let mut cpu = CPU::new();
+        cpu.mem[0x0400] = 0xEA; // NOP
+        cpu.mem[0x0401] = 0x20; // JSR 0x0405
+        cpu.mem[0x0402] = 0x04;
+        cpu.mem[0x0403] = 0x05;
+        cpu.mem[0x0004] = 0x00; // BRK
+        cpu.mem[0x0405] = 0xA2; // LDX #$65
+        cpu.mem[0x0406] = 0x65;
+        cpu.mem[0x0407] = 0x86; // STX $05
+        cpu.mem[0x0408] = 0x05;
+        cpu.mem[0x0409] = 0xA2; // LDX #$02
+        cpu.mem[0x040A] = 0x02;
+        cpu.mem[0x040B] = 0x86; // STX $06
+        cpu.mem[0x040C] = 0x06;
+        cpu.mem[0x040D] = 0x60; // RTS
+        cpu.run();
+        assert_eq!(0x65, cpu.mem[0x0005]);
+        assert_eq!(0x02, cpu.mem[0x0006]);
     }
 }
